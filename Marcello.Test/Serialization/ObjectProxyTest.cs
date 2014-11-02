43d1e991862c0546c4205b4b1a5444935a451bfd
Mark Meeus <mark.meeus@gmail.com>
2014-11-02 19:03:59 +0100

    More code cleanup

--- Marcello.Test/Serialization/ObjectProxyTest.cs
+++ Marcello.Test/Serialization/ObjectProxyTest.cs
@@ -4,54 +4,53 @@ using Marcello.Serialization;
 
 namespace Marcello.Test.Serialization
 {   
-    [TestFixture ()]
+    [TestFixture]
     public class ObjectProxyTest
     {
-
-        [Test()]
+        [Test]
         public void TestReturnsNullIfNoID()
         {
             Assert.AreEqual(null, new ObjectProxy(new {name = "123"}).ID, "Should return null when no ID");
         }
             
-        [Test()]
+        [Test]
         public void TestShouldReturnID()
         {
             Assert.AreEqual(1, new ObjectProxy(new ClassWithID(){ID = 1}).ID, "Should return null when no ID");
         }
 
-        [Test()]
+        [Test]
         public void TestShouldReturnId()
         {
             Assert.AreEqual(1, new ObjectProxy(new ClassWithId(){Id = 1}).ID, "Should return null when no ID");
         }
 
-        [Test()]
+        [Test]
         public void TestShouldReturnid()
         {
             Assert.AreEqual(1, new ObjectProxy(new ClassWithid(){id = 1}).ID, "Should return null when no ID");
         }
 
 
-        [Test()]
+        [Test]
         public void TestShouldReturnClassID()
         {
             Assert.AreEqual(1, new ObjectProxy(new ClassWithClassID(){ClassWithClassIDID = 1}).ID, "Should return null when no ID");
         }
 
-        [Test()]
+        [Test]
         public void TestShouldReturnClassId()
         {
             Assert.AreEqual(1, new ObjectProxy(new ClassWithClassId(){ClassWithClassIdId = 1}).ID, "Should return null when no ID");
         }
 
-        [Test()]
+        [Test]
         public void TestShouldReturnClassid()
         {
             Assert.AreEqual(1, new ObjectProxy(new ClassWithClassid(){ClassWithClassidid = 1}).ID, "Should return null when no ID");
         }
 
-        [Test()]
+        [Test]
         public void TestShouldReturnAttributedId()
         {
             Assert.AreEqual(1, new ObjectProxy(new ClassWithAttrID(){AttributedID = 1}).ID, "Should return null when no ID");
@@ -59,4 +58,3 @@ namespace Marcello.Test.Serialization
 
     }
 }
-
