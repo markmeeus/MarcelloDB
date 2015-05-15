a3ad0eee83dff26bb843b927f054381c7553af1f
Mark Meeus <mark.meeus@gmail.com>
2015-05-15 19:53:24 +0200

    Remove unused StorageEngine base class

--- MarcelloDB/Storage/StorageEngine.cs
+++ MarcelloDB/Storage/StorageEngine.cs
@@ -5,63 +5,34 @@ using MarcelloDB.Transactions.__;
 
 namespace MarcelloDB.Storage
 {
-    public abstract class StorageEngine
-    {
-        internal abstract byte[] Read (long address, int length);
-
-        internal abstract void Write (long address, byte[] bytes);
-
-        protected bool JournalEnabled { get; set; }
-
-        internal void DisableJournal()
-        {
-            JournalEnabled = false;
-        }       
-    }
-
-    public class StorageEngine<T> : StorageEngine
+    internal class StorageEngine<T>
     {
         internal Marcello Session { get; set; }
 
         public StorageEngine(Marcello session)
         {
             Session = session;
-            JournalEnabled = typeof(T) != typeof(TransactionJournal);
         }
 
-        internal override byte[] Read(long address, int length)
+        internal byte[] Read(long address, int length)
         {
             return Reader().Read(address, length);
         }
                    
-        internal override void Write(long address, byte[] bytes)
+        internal void Write(long address, byte[] bytes)
         {
             Writer().Write(address, bytes);
         }                   
             
         #region reader/writer factories
         Writer Writer()
-        {
-            if (JournalEnabled) 
-            {    
-                return new JournalledWriter(this.Session, typeof(T).Name);
-            }
-            else 
-            {
-                return new Writer(this.Session, typeof(T).Name);
-            }
+        {            
+            return new JournalledWriter(this.Session, typeof(T).Name);
         }
 
         Reader Reader()
-        {
-            if (JournalEnabled) 
-            {    
-                return new JournalledReader(this.Session, typeof(T).Name);
-            }
-            else 
-            {
-                return new Reader(this.Session, typeof(T).Name);
-            }
+        {            
+            return new JournalledReader(this.Session, typeof(T).Name);
         }
         #endregion 
     }
