4f3064188a83af59326eef4c3465924979d6bc1a
Mark Meeus <mark.meeus@gmail.com>
2015-05-26 21:13:30 +0200

    A few more benchmarks

--- MarcelloDB.BenchmarkTool/Program.cs
+++ MarcelloDB.BenchmarkTool/Program.cs
@@ -6,30 +6,45 @@ namespace MarcelloDB.BenchmarkTool
     class MainClass
     {
         public static void Main(string[] args)
-        {   
+        {              
+            TimeSpan elapsed;
             foreach (var objectCount in new int[]{500, 1000, 2000, 4000, 8000})
             {
-                var elapsed = new SequentialIDsBulkInsert(objectCount).Run();
+                elapsed = new SequentialIDsBulkInsert(objectCount).Run();
                 Console.WriteLine(string.Format("SequentialIDsBulkInsert {0} took: {1} ms", objectCount, elapsed.TotalMilliseconds));
             }
 
             foreach (var objectCount in new int[]{500, 1000, 2000, 4000, 8000})
             {
-                var elapsed = new RandomIDsBulkInsert(objectCount).Run();
+                elapsed = new RandomIDsBulkInsert(objectCount).Run();
                 Console.WriteLine(string.Format("RandomIDsBulkInsert {0} took: {1} ms", objectCount, elapsed.TotalMilliseconds));
             }
 
             foreach (var objectCount in new int[]{500, 1000, 2000, 4000, 8000})
             {
-                var elapsed = new RandomRead(objectCount).Run();
+                elapsed = new RandomRead(objectCount).Run();
                 Console.WriteLine(string.Format("RandomRead {0} took: {1} ms", objectCount, elapsed.TotalMilliseconds));
             }
 
             foreach (var objectCount in new int[]{500, 1000, 2000, 4000, 8000})
             {
-                var elapsed = new EnumerateAll(objectCount).Run();
-                Console.WriteLine(string.Format("RandomRead {0} took: {1} ms", objectCount, elapsed.TotalMilliseconds));
+                elapsed = new EnumerateAll(objectCount).Run();
+                Console.WriteLine(string.Format("EnumerateAll {0} took: {1} ms", objectCount, elapsed.TotalMilliseconds));
             }
+
+            int large = 100 * 1000;
+            elapsed = new SequentialIDsBulkInsert(large).Run();
+            Console.WriteLine(string.Format("SequentialIDsBulkInsert {0} took: {1} ms", large, elapsed.TotalMilliseconds));
+
+            elapsed = new RandomIDsBulkInsert(large).Run();
+            Console.WriteLine(string.Format("RandomIDsBulkInsert {0} took: {1} ms", large, elapsed.TotalMilliseconds));
+
+            elapsed = new RandomRead(large).Run();
+            Console.WriteLine(string.Format("RandomRead {0} took: {1} ms", large, elapsed.TotalMilliseconds));
+
+            elapsed = new EnumerateAll(large).Run();
+            Console.WriteLine(string.Format("EnumerateAll {0} took: {1} ms", large, elapsed.TotalMilliseconds));
+
         }
     }
 }
