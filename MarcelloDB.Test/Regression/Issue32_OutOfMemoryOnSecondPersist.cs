21904206eacb99ae794d0eb6bfe8a8099ee3e889
markmeeus <mark.meeus@gmail.com>
2016-04-10 09:08:53 +0200

    Save Root record if it got updated while saving it. Relates to issue #32

---
+++ MarcelloDB.Test/Regression/Issue32_OutOfMemoryOnSecondPersist.cs
@@ -0,0 +1,47 @@
+﻿using MarcelloDB.Collections;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MarcelloDB.Test.Regression.Issue32
+{
+    public class Product
+    {
+        public Guid Id { get; set; }
+    }
+
+    [TestFixture]
+    public class Issue32_OutOfMemoryOnSecondPersist
+    {
+        TestPlatform _platform;
+
+        [SetUp]
+        public void Setup()
+        {
+            _platform = new TestPlatform();
+        }
+
+        [Test]
+        public void Should_Not_Throw_On_Second_Persist()
+        {
+            SaveProduct();
+            Assert.DoesNotThrow(SaveProduct);
+        }
+
+        void SaveProduct()
+        {
+            using(var session = new Session(_platform, "/"))
+            {
+                var collectionFile = session["products"];
+                var products = collectionFile.Collection<Product>("products");
+
+                var product = new Product();
+                product.Id = Guid.NewGuid();
+                products.Persist(product);
+            }
+        }
+    }
+}
