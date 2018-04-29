1a6e88d9b2919f98ba77a42d8a54be84d95c66b6
Mark Meeus <mark.meeus@gmail.com>
2018-04-29 10:16:14 +0200

    Fix integer overflow on large collection files

--- MarcelloDB/Helpers/DataHelper.cs
+++ MarcelloDB/Helpers/DataHelper.cs
@@ -10,30 +10,30 @@ namespace MarcelloDB.Helpers
             Int64 targetAddress,
             byte[] targetData)
         {
-            var lengthToCopy = sourceData.Length;
-            var sourceIndex = 0;
-            var targetIndex = 0;
+            Int64 lengthToCopy = sourceData.Length;
+            Int64 sourceIndex = 0;
+            Int64 targetIndex = 0;
 
             if (sourceAddress < targetAddress) 
             {
-                sourceIndex = (int)(targetAddress - sourceAddress);
+                sourceIndex = (targetAddress - sourceAddress);
                 lengthToCopy = sourceData.Length - sourceIndex;
             }
 
             if (sourceAddress > targetAddress) 
             {
-                targetIndex = (int)(sourceAddress - targetAddress);
+                targetIndex = (sourceAddress - targetAddress);
                 lengthToCopy = targetData.Length - targetIndex;
             }
 
             //max length to copy to not overrun the target array
             lengthToCopy = Math.Min(lengthToCopy, targetData.Length - targetIndex);
-            //max length to copy to not overrrude the source array
+            //max length to copy to not overrrun the source array
             lengthToCopy = Math.Min(lengthToCopy, sourceData.Length - sourceIndex);
 
             if (lengthToCopy > 0) 
             {
-                Array.Copy(sourceData, sourceIndex, targetData, targetIndex, lengthToCopy);
+                Array.Copy(sourceData, (int)sourceIndex, targetData, (int)targetIndex, (int)lengthToCopy);
             }
 
         }
