a1a07b988ec8f4e121fb498dc870a53858419e12
markmeeus <mark.meeus@gmail.com>
2020-02-03 18:03:59 +0100

    bump to 1.0.9

--- build_package.ps1
+++ build_package.ps1
@@ -1,5 +1,5 @@
 
-C:\"Program Files (x86)"\"Microsoft Visual Studio"\2017\Community\MSBuild\15.0\Bin\MSBuild.exe .\MarcelloDB.uwp.sln
+//D:\"Visual Studio"\MSBuild\Current\Bin\MSBuild.exe .\MarcelloDB.uwp.sln
 
 Remove-Item -Recurse -Force Package\lib
 mkdir Package\lib
@@ -14,29 +14,29 @@ mkdir Package\lib\portable-net45+win+wp80+wp81+MonoTouch10+MonoAndroid10+xamarin
 mkdir Package\lib\netstandard1.1
 mkdir Package\lib\uap10.0
 
-cp MarcelloDB\bin\debug\MarcelloDB.dll Package\lib\net45\MarcelloDB.dll
-cp MarcelloDB.netfx\bin\debug\MarcelloDB.netfx.dll Package\lib\net45\MarcelloDB.netfx.dll
+cp MarcelloDB\bin\release\MarcelloDB.dll Package\lib\net45\MarcelloDB.dll
+cp MarcelloDB.netfx\bin\release\MarcelloDB.netfx.dll Package\lib\net45\MarcelloDB.netfx.dll
 
-cp MarcelloDB\bin\debug\MarcelloDB.dll Package\lib\MonoAndroid\MarcelloDB.dll
-cp MarcelloDB.netfx\bin\debug\MarcelloDB.netfx.dll Package\lib\MonoAndroid\MarcelloDB.netfx.dll
+cp MarcelloDB\bin\release\MarcelloDB.dll Package\lib\MonoAndroid\MarcelloDB.dll
+cp MarcelloDB.netfx\bin\release\MarcelloDB.netfx.dll Package\lib\MonoAndroid\MarcelloDB.netfx.dll
 
-cp MarcelloDB\bin\debug\MarcelloDB.dll Package\lib\MonoTouch\MarcelloDB.dll
-cp MarcelloDB.netfx\bin\debug\MarcelloDB.netfx.dll Package\lib\MonoTouch\MarcelloDB.netfx.dll
+cp MarcelloDB\bin\release\MarcelloDB.dll Package\lib\MonoTouch\MarcelloDB.dll
+cp MarcelloDB.netfx\bin\release\MarcelloDB.netfx.dll Package\lib\MonoTouch\MarcelloDB.netfx.dll
 
-cp MarcelloDB\bin\debug\MarcelloDB.dll Package\lib\Xamarin.iOS\MarcelloDB.dll
-cp MarcelloDB.netfx\bin\debug\MarcelloDB.netfx.dll Package\lib\Xamarin.iOS\MarcelloDB.netfx.dll
+cp MarcelloDB\bin\release\MarcelloDB.dll Package\lib\Xamarin.iOS\MarcelloDB.dll
+cp MarcelloDB.netfx\bin\release\MarcelloDB.netfx.dll Package\lib\Xamarin.iOS\MarcelloDB.netfx.dll
 
-cp MarcelloDB\bin\debug\MarcelloDB.dll Package\lib\Xamarin.Mac\MarcelloDB.dll
-cp MarcelloDB.netfx\bin\debug\MarcelloDB.netfx.dll Package\lib\Xamarin.Mac\MarcelloDB.netfx.dll
+cp MarcelloDB\bin\release\MarcelloDB.dll Package\lib\Xamarin.Mac\MarcelloDB.dll
+cp MarcelloDB.netfx\bin\release\MarcelloDB.netfx.dll Package\lib\Xamarin.Mac\MarcelloDB.netfx.dll
 
-cp MarcelloDB\bin\debug\MarcelloDB.dll Package\lib\portable-netcore451+wpa81\MarcelloDB.dll
+cp MarcelloDB\bin\release\MarcelloDB.dll Package\lib\portable-netcore451+wpa81\MarcelloDB.dll
 
-cp MarcelloDB\bin\debug\MarcelloDB.dll Package\lib\portable-net45+win+wp80+wp81+MonoTouch10+MonoAndroid10+xamarinmac20+xamarinios10\MarcelloDB.dll
+cp MarcelloDB\bin\release\MarcelloDB.dll Package\lib\portable-net45+win+wp80+wp81+MonoTouch10+MonoAndroid10+xamarinmac20+xamarinios10\MarcelloDB.dll
 
-cp MarcelloDB\bin\debug\MarcelloDB.dll Package\lib\netstandard1.1\MarcelloDB.dll
+cp MarcelloDB\bin\release\MarcelloDB.dll Package\lib\netstandard1.1\MarcelloDB.dll
 
-cp MarcelloDB\bin\debug\MarcelloDB.dll Package\lib\uap10.0\MarcelloDB.dll
-cp MarcelloDB.uwp\bin\debug\MarcelloDB.uwp.dll Package\lib\uap10.0\MarcelloDB.uwp.dll
+cp MarcelloDB\bin\release\MarcelloDB.dll Package\lib\uap10.0\MarcelloDB.dll
+cp MarcelloDB.uwp\bin\release\MarcelloDB.uwp.dll Package\lib\uap10.0\MarcelloDB.uwp.dll
 
 cp BuildArtifacts\wpa81\MarcelloDB.W81.dll Package\lib\wpa81\MarcelloDB.W81.dll
 
